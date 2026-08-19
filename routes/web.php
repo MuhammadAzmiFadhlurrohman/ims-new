66479729e8a3ea720c06fc338e8ddc926072f7fa
MuhammadAzmiFadhlurrohman <azmigudir@gmail.com>
2026-08-19 20:46:20 +0700

    feat: auto redirect root URL to /admin

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 Route::middleware(['web', 'auth'])->group(function () {
