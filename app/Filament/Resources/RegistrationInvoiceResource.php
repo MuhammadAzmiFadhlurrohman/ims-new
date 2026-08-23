6c4ff2494ad111ae5ec3d643fc0649a0d509a0c5
MuhammadAzmiFadhlurrohman <azmigudir@gmail.com>
2026-08-23 20:49:29 +0700

    fix(invoices): import Str helper in RegistrationInvoiceResource

--- app/Filament/Resources/RegistrationInvoiceResource.php
+++ app/Filament/Resources/RegistrationInvoiceResource.php
@@ -13,6 +13,7 @@ use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 
 class RegistrationInvoiceResource extends Resource
 {
