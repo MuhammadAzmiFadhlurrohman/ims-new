de4b037b63aac96f61a8753ffcc7aa5a231b0de5
MuhammadAzmiFadhlurrohman <azmigudir@gmail.com>
2026-08-25 13:11:00 +0700

    feat(maps): apply Google Maps roadmap tiles to Admin Dashboard stats map and Landing page coverage map

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -371,9 +371,10 @@
                             attributionControl: false
                         });
 
-                        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-                            maxZoom: 19,
-                            subdomains: 'abcd',
+                        // Google Maps Roadmap tile layer
+                        L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+                            maxZoom: 20,
+                            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
                         }).addTo(this.mapInstance);
 
                         this.markersLayer = L.layerGroup().addTo(this.mapInstance);
