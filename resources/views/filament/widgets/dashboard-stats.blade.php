29c5fd4b11cfa976204f7f94d4f5c4de011000c3
MuhammadAzmiFadhlurrohman <azmigudir@gmail.com>
2026-08-25 13:40:45 +0700

    feat(maps): apply Google Maps roadmap tiles with standard 256px tile size to Admin Dashboard stats map and Coverage page

--- resources/views/filament/widgets/dashboard-stats.blade.php
+++ resources/views/filament/widgets/dashboard-stats.blade.php
@@ -96,10 +96,11 @@
                     attributionControl: false
                 });
 
-                // High reliability tile layer (CartoDB Voyager)
-                L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-                    maxZoom: 19,
-                    subdomains: 'abcd',
+                // Google Maps Roadmap tile layer
+                L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+                    maxZoom: 20,
+                    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                    tileSize: 256
                 }).addTo(this.mapInstance);
 
                 this.markersLayer = L.layerGroup().addTo(this.mapInstance);
