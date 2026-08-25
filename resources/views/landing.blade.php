56634ff30778e7ecebc0a3db6569c5dc8ec5a671
MuhammadAzmiFadhlurrohman <azmigudir@gmail.com>
2026-08-25 13:21:51 +0700

    feat(landing): apply Google Maps roadmap tiles on landing page coverage map

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -371,9 +371,10 @@
                             attributionControl: false
                         });
 
-                        L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-                            maxZoom: 19,
-                            subdomains: 'abcd',
+                        L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+                            maxZoom: 20,
+                            subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+                            tileSize: 256,
                         }).addTo(this.mapInstance);
 
                         this.markersLayer = L.layerGroup().addTo(this.mapInstance);
